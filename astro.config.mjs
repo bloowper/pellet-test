7811c6382a02e9f8d2106994105f011e9cb0685e
Tomasz Orchowski <tomaszorchowski99@gmail.com>
2025-08-03 18:15:33 +0200

    Update deployment workflow and modify Astro config
    Enhanced the GitHub Actions workflow to improve deployment concurrency, package manager detection, and compatibility with Node.js 20. Updated Astro configuration to reflect new base path for GitHub Pages deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // Set the base path for GitHub Pages deployment
-  // Replace 'your-repo-name' with the name of your GitHub repository
   site: 'https://bloowper.github.io',
-  base: '/eminent-ephemera',
+  base: '/pellet-test',
 });
